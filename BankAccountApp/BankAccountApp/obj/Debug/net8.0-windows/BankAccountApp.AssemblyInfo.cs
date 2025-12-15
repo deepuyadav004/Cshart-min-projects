240a4e3a0becaf269b10284a330ece90f93e64bb
deepuyadav004 <deepuyadavze@gmail.com>
2025-12-15 16:29:54 +0530

    minimal crud

--- BankAccountApp/BankAccountApp/obj/Debug/net8.0-windows/BankAccountApp.AssemblyInfo.cs
+++ BankAccountApp/BankAccountApp/obj/Debug/net8.0-windows/BankAccountApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankAccountApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff9b90c5f1d96afc62a5688951af37e235a1656")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankAccountApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankAccountApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
